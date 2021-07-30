47d0da2f3516eeb57680c60780a38e2671737f9c
Subbotina Eugenia <63358524+jenyasubbotina@users.noreply.github.com>
2021-07-30 14:21:43 +0500

    New route for creating user

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const AnimalType = require('../models/animal_type')
+const User = require('../models/user')
+const userControls = require('../controls/user_controls');
 
 // Logic
 router.get('/getAvailablePetTypes', (req, res) => {
     AnimalType.find()
-            .then(data => res.send(data))
-            .catch(err => console.error(err));
+    .then(data => res.send(data))
+    .catch(err => console.error(err));
     
 });
 
@@ -17,11 +19,18 @@ router.post('/addPetType', (req, res) => {
         imgURL : req.body.imgURL
     });
     newAnimalType.save()
-                .then(data => {
-                    res.send(data);
-                })
-                .catch(err => console.error(err));
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => console.error(err));
 });
 
+router.post('/createUser', register);
+
+function register(req, res, next) {
+    userControls.create(req.body)
+        .then(() => res.json({message : "User created"}))
+        .catch(err => next(err));
+}
 
 module.exports = router;
